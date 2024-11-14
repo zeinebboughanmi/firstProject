fc75cc72309f98323a91ecc6104b33951a40ff2c
zeinebboughanmi <zeinebboughanmi89@gmail.com>
2024-11-14 03:16:46 +0100

    new projet

---
+++ main.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function() {
+    var total = 0;
+
+  
+    $("#btn1").click(function() {
+        total += 50;
+        $("#totala").text(total);
+    });
+
+    $("#btn2").click(function() {
+        total += 60;
+        $("#totala").text(total);
+    });
+
+    $("#btn3").click(function() {
+        total += 80;
+        $("#totala").text(total);
+    });
+
+    $("#btn4").click(function() {
+        total += 100;
+        $("#totala").text(total);
+    });
+
+    $("#btn5").click(function() {
+        total += 50;
+        $("#totala").text(total);
+    });
+
+    $("#btn6").click(function() {
+        total += 60;
+        $("#totala").text(total);
+    });
+
+    $("#btn7").click(function() {
+        total += 80;
+        $("#totala").text(total);
+    });
+
+    $("#btn8").click(function() {
+        total += 100;
+        $("#totala").text(total);
+    });
+});
+
+
